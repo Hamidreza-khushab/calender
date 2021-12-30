5abbd5b2d80e851f2525f7f0afab62daed71a952
Hamidreza <qiisohamid@gmail.com>
2021-12-30 18:43:59 +0100

    navbar start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navapp from './components/nav/Nav'
+import { Container } from "react-bootstrap";
+import './App.css';
+
+function App() {
+  return (
+    <Navapp />
+
+  );
+}
+
+export default App;
